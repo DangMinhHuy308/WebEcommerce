7aae397d1c62d3d00500344da43d82433e92ccbb
DangMinhHuy308 <huyminhdang5@gmail.com>
2024-10-21 10:56:47 +0700

    admin page

--- WebEcommerce/Program.cs
+++ WebEcommerce/Program.cs
@@ -1,8 +1,10 @@
+using AspNetCoreHero.ToastNotification;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddNotyf(config => { config.DurationInSeconds = 10; config.IsDismissable = true; config.Position = NotyfPosition.BottomRight; });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +22,9 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapControllerRoute(
+    name: "area",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
